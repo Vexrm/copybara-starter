b8e253f3b2c80f4de37b88b0b49e15fba82b4c82
David Lowe <mercy-virtual+dl@asynchrony.com>
2017-09-05 12:22:12 -0500

    I was asked for an example of a variable file

--- spec/basetest/landing_spec.rb
+++ spec/basetest/landing_spec.rb
@@ -1,10 +1,13 @@
 require 'spec_helper'
+require 'searchterms'
 
 describe 'Country Search' do
     it 'should go to google and search country ' do
         visit "https://www.google.com"
-        fill_in "q", with:"Country"
+        ranSearch = rand(4)
+        fill_in "q", with: $searchTerm[ranSearch]
         find_field("q").send_keys :enter
-        expect(page).to have_text "United States"
+        expect(page).to have_text $searchTerm[ranSearch] #bad test as it could just be on the search bar
+        #The dollar sign says this variable is from another file. See searchterms.rb
     end
 end
---
+++ spec/searchterms.rb
@@ -0,0 +1,2 @@
+$searchTerm = Array.[]("Country", "Steve", "Soylent", "Bread Co")
+# The $ goes "This variable will be used outside this file" 
